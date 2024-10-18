81635e426a20144d0bc860e25c2c6582cf7894d6
ArtemPich <ksedoy@gmail.com>
2024-10-18 20:28:00 +0300

    Second

--- first-project/first_project/app/views.py
+++ first-project/first_project/app/views.py
@@ -1,30 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
 
-
 def home_view(request):
     template_name = 'app/home.html'
-    # впишите правильные адреса страниц, используя
-    # функцию `reverse`
+
     pages = {
         'Главная страница': reverse('home'),
         'Показать текущее время': reverse('current_time'),
         'Показать содержимое рабочей директории': reverse('workdir')
     }
     
-    # context и параметры render менять не нужно
-    # подбробнее о них мы поговорим на следующих лекциях
     context = {
         'pages': pages
     }
     return render(request, template_name, context)
 
-
 def time_view(request):
     current_time = None
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
-
 
 def workdir_view(request):
     workdir = None
