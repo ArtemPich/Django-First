f2784e094a45fd469f382e57066eb5562a4ececd
ArtemPich <ksedoy@gmail.com>
2024-10-20 13:26:06 +0300

    3 part

--- first-project/first_project/app/views.py
+++ first-project/first_project/app/views.py
@@ -1,12 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render, reverse
+from datetime import datetime
+import os
 
 def home_view(request):
     template_name = 'app/home.html'
 
     pages = {
         'Главная страница': reverse('home'),
-        'Показать текущее время': reverse('current_time'),
+        'Показать текущее время': reverse('time'),
         'Показать содержимое рабочей директории': reverse('workdir')
     }
     
@@ -16,13 +18,11 @@ def home_view(request):
     return render(request, template_name, context)
 
 def time_view(request):
-    current_time = None
-    msg = f'Текущее время: {current_time}'
-    return HttpResponse(msg)
+    current_time = datetime.today().strtime('%H:%M:%S')
+    return HttpResponse(f'Текущее время: {current_time}')
 
 def workdir_view(request):
-    workdir = None
-    msg = f'Содержимое рабочей директории: {workdir}'
-    return HttpResponse(msg)
+    files_project = os.listdir(os.getcwd())
+    return HttpResponse(f'Список файлов рабочей директории: {files_project}')
 
-    raise NotImplemented
+
